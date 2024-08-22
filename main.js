2c9c31b8b2995de682ac5065ebac46372432bcf6
abduaizm <safasff@gmail.com>
2024-08-22 16:46:02 +0500

    save

---
+++ main.js
@@ -0,0 +1,130 @@
+let id = {
+    name1: prompt('Введите имя'),
+    age1: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 1');
+console.log('Имя - ' + id.name1);
+console.log('Возрост - ' + id.age1);
+
+
+
+
+let id2 = {
+    name2: prompt('Введите имя'),
+    age2: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 2');
+console.log('Имя - ' + id2.name2);
+console.log('Возрост - ' + id2.age2);
+
+
+
+
+let id3 = {
+    name3: prompt('Введите имя'),
+    age3: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 3');
+console.log('Имя - ' + id3.name3);
+console.log('Возрост - ' + id3.age3);
+
+
+
+
+let id4 = {
+    name4: prompt('Введите имя'),
+    age4: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 4');
+console.log('Имя - ' + id4.name4);
+console.log('Возрост - ' + id4.age4);
+
+
+
+
+let id5 = {
+    name5: prompt('Введите имя'),
+    age5: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 5');
+console.log('Имя - ' + id5.name5);
+console.log('Возрост - ' + id5.age5);
+
+
+
+
+
+
+let id6 = {
+    name6: prompt('Введите имя'),
+    age6: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 6');
+console.log('Имя - ' + id6.name6);
+console.log('Возрост - ' + id6.age6);
+
+
+
+
+
+
+let id7 = {
+    name7: prompt('Введите имя'),
+    age7: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 7');
+console.log('Имя - ' + id7.name7);
+console.log('Возрост - ' + id7.age7);
+
+
+
+
+
+
+let id8 = {
+    name8: prompt('Введите имя'),
+    age8: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 8');
+console.log('Имя - ' + id8.name8);
+console.log('Возрост - ' + id8.age8);
+
+
+
+
+
+
+
+
+
+let id9 = {
+    name9: prompt('Введите имя'),
+    age9: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 9');
+console.log('Имя - ' + id9.name9);
+console.log('Возрост - ' + id9.age9);
+
+
+
+
+
+
+
+let id10 = {
+    name10: prompt('Введите имя'),
+    age10: +prompt('Введите возрост')
+} 
+
+console.log('Пользователь - 10');
+console.log('Имя - ' + id10.name10);
+console.log('Возрост - ' + id10.age10);
